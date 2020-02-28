b946f00b6df49fae40179aef849cce3330395dd9
Alex <abiz@ucsd.edu>
2020-02-27 21:25:09 -0800

    finished

--- public/js/introHCI.js
+++ public/js/introHCI.js
@@ -10,8 +10,8 @@ $(document).ready(function() {
  */
 function initializePage() {
 	console.log("Page ready");
- 	// initCamera();
- 	// initMap();
+ 	initCamera();
+ 	initMap();
  	initGestures();
  	initRSVPForm();
 }
@@ -23,5 +23,17 @@ function initGestures() {
 
 // init RSVP form submit listener
 function initRSVPForm() {
-  // add your code here
+  $('#rsvpForm').submit(function (e) {
+
+	e.preventDefault();
+	console.log('submitting form...');
+	var rsvpEmail = $('#rsvpEmail').val();
+	$.post('addRSVP', {rsvpEmail: rsvpEmail}, postCallback);
+
+  });
+
+  function postCallback(res){
+	  alert("RSVP form successfully submitted!");
+	  $('#rsvpEmail').val('');
+  }
 }
--- public/js/mapQuest.js
+++ public/js/mapQuest.js
@@ -1,3 +1,12 @@
 function initMap() {
-	// add your code here
+	L.mapquest.key = '5gjgHte9lENdAyGpnVid7GjTvGfGQTXA';
+
+// 'map' refers to a <div> element with the ID map
+var map = L.mapquest.map('map', {
+  center: [37.7749, -122.4194],
+  layers: L.mapquest.tileLayer('map'),
+  zoom: 12
+});
+
+L.marker([32.48, -117.236]).addTo(map);
 }
